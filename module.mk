dc741c9c81c0560dc0ae5fd39fd97a0212e3dd9d
kmarkley86 <kmarkley86@d7d0ee8e-a184-4e07-bb8d-9c3438fa7d71>
2016-01-06 07:05:38 +0000

    Version 1.1.8.
    git-svn-id: http://svn.code.sf.net/p/opaque-typedef/code/trunk@56 d7d0ee8e-a184-4e07-bb8d-9c3438fa7d71

--- module.mk
+++ module.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2015
+# Copyright (c) 2015, 2016
 # Kyle Markley.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -28,7 +28,7 @@
 #
 
 NAME = opaque
-VERSION = 1.1.7
+VERSION = 1.1.8
 
 COMMON += -Iinclude
 COMMON += -std=c++11 -pedantic
